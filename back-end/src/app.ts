81d065bab8a0cb9c8bfee19508214a1ac8da3b42
Andrey Segobia <andrey.segobia.prol@gmail.com>
2026-07-31 20:54:33 -0400

    feat: estrutura inicial do back-end com Express e TypeScript

---
+++ back-end/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import type { Request, Response} from 'express';
+import dotenv from 'dotenv';
+import { expectFailure } from 'node:test';
+import chalk from 'chalk'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Forma do express interpretar aquivos json.
+app.use(express.json());
+
+//Rota de teste paraconfirmar que servidor está rodando,
+app.get('/health', (req: Request, res: Response) => {
+    res.json({status: 'ok'});
+});
+
+app.listen(PORT, () => {
+    console.log(chalk.green.bgBlack.italic(` Server running on port => ${PORT} `))
+});
+
+export default app;
