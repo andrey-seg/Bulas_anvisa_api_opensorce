6b36fe97c888a100af99d13b56b39bf9da01e10d
Andrey Segobia <andrey.segobia.prol@gmail.com>
2026-08-07 22:15:46 -0400

    refactor: remover estrutura back-end e mover código para a raiz

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+//Deixar importação de dependencias npm aqui.
 import express from 'express';
 import type { Request, Response} from 'express';
 import dotenv from 'dotenv';
-import { expectFailure } from 'node:test';
 import chalk from 'chalk'
+
+//deixar importação de itens node aqui.
+import { expectFailure } from 'node:test';
+import { error } from 'node:console';
+
+//Deixar importação de funções de aquivos aqui.
 import serchForMedication from './integrations/anvisa.js';
+
 
 dotenv.config();
 
@@ -19,9 +26,18 @@ app.get('/health', (req: Request, res: Response) => {
 });
 
 app.get('/medication', async (req: Request, res: Response) => {
-   const name = req.query.name;
-   
-})
+
+   try{const name = req.query.name as string;
+    //buscar resultado em anvisa.ts
+   const result = await serchForMedication(name);
+    //devolução da resposta em formato json.
+   res.json(result);} 
+    
+    catch(error){
+        console.log(chalk.red(`Erro => ${error}`))
+        res.status(500).json({erro: `Falha ao buscar medicamento`})
+    };
+});
 
 app.listen(PORT, () => {
     console.log(chalk.green.bgBlack.italic(` Server running on port => ${PORT} `))
